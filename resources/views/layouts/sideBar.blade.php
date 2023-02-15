151e6163d22da90c9826c33decf6154fec099946
fajariadi <fajariadi.19@gmail.com>
2023-02-15 09:41:42 +0700

    edit sideBar

--- resources/views/layouts/sideBar.blade.php
+++ resources/views/layouts/sideBar.blade.php
@@ -14,7 +14,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+          <li class="nav-item">
             <a href="{{route('dashboard')}}" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
@@ -22,16 +22,43 @@
               </p>
             </a>
           </li>
-          {{-- <li class="nav-item">
+          <li class="nav-item">
             <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+              <i class="fa fa-book"></i>
               <p>
-                Widgets
+                Buku
                 <span class="right badge badge-danger">New</span>
               </p>
             </a>
           </li>
           <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fa fa-book"></i>
+              <p>
+                Peminjaman
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fa fa-book"></i>
+              <p>
+                Penjualan
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fa fa-book"></i>
+              <p>
+                Anggota
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+        {{--   <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
